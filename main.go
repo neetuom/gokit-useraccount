35b8ce00fd639bce5401b9cfd31483d4f8f459d9
arshita <varunanand@Varuns-MacBook-Pro.local>
2022-12-19 23:01:20 +0530

    changes in file

--- main.go
+++ main.go
@@ -51,6 +51,9 @@ func dsn(dbName string) string {
 
 func main() {
 
+     /**
+      *  Logger and http variables declaration
+     */
      var httpAddr = flag.String("http",":8088","http listen address")
 
      var logger log.Logger
@@ -64,8 +67,15 @@ func main() {
      		)
      	}
 
+     /**
+      *  Service service started logging in logger.
+     */
      level.Info(logger).Log("msg", "service started")
 
+
+     /**
+      *  Defer the service message logger till program runs.
+     */
      defer level.Info(logger).Log("msg", "service ended")
 
 
@@ -74,9 +84,7 @@ func main() {
      var db *sql.DB
      	{
      		var err error
-
      		db, err = sql.Open("mysql", dbsource)  // db, err = sql.Open("postgres", dbsource)   - PostgreSQL
-
      		if err != nil {
      			level.Error(logger).Log("exit", err)
      			os.Exit(-1)
@@ -85,37 +93,59 @@ func main() {
      */
 
 
-     //Syntax 2 - To open DB connection
+        //Syntax 2 - To open DB connection
         db, err := sql.Open("mysql", dsn(dbname))
         if err != nil {
              			level.Error(logger).Log("exit", err)
              			os.Exit(-1)
         }
 
-
+        /**
+         *  Enables one to parse command-line flags.
+        */
      	flag.Parse()
 
+        /**
+          *  Background returns a non-nil, empty Context.
+        */
      	ctx := context.Background()
 
-
+        /**
+          *  Service variable srv initialization.
+          *
+          *  Curly brackets on their own define lexical scope in Go. Meaning, whatever happens in that scope, stays in that scope.
+          *  Variable declarations, calculations, etc.
+        */
      	var srv account.Service
      	{
      		repository := account.NewRepo(db, logger)
      		srv = account.NewService(repository, logger)
      	}
 
-
+        /**
+          *  errs chanel declarations. Goroutines communicate using channels.
+          *  Channels can be thought of as a pipe using which Goroutines communicate.
+        */
      	errs := make(chan error)
 
+
+        /**
+          *  Goroutine to send the program interruption call to send errs channel.
+        */
      	go func() {
      		c := make(chan os.Signal, 1)
      		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
      		errs <- fmt.Errorf("%s", <-c)
      	}()
 
-
+        /**
+          *  Endpoints initialized
+        */
      	endpoints := account.MakeEndpoints(srv)
 
+        /**
+          *  Goroutine to start the http server
+        */
      	go func() {
      		fmt.Println("listening on port", *httpAddr)
      		handler := account.NewHTTPServer(ctx, endpoints)
